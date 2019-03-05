4f58486e1a17e8da362e0453e7db55ccb6704e17
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-03-05 18:44:56 +0100

    Modified the script in order to complete the 'optional' task!

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -12,6 +12,7 @@ button.addEventListener("click", () => {
     generateGrid(dimension);    
 })
 
+//Here are declared the functions to run the main code
 function generateGrid (number){
 
     if(document.contains(document.querySelector(".container"))){
@@ -23,8 +24,16 @@ function generateGrid (number){
     container.setAttribute("style", "grid-template: repeat(" + number + ", 1fr) / repeat(" + number + ",1fr);")
 
     for(let i = 0; i < number * number; i++){
+
         let box = document.createElement("div");
-        box.setAttribute("class", "box")
+        box.setAttribute("class", "box");
+        box.setAttribute("style", "background : white");
+
+        let blackOverlay = document.createElement("div");
+        blackOverlay.setAttribute("class", "overlay");
+        blackOverlay.setAttribute("style","width: 100%; height: 100%; background-color: black; opacity: 0;");
+        box.appendChild(blackOverlay);
+
         container.appendChild(box);
         boxes.push(box);
     }
@@ -34,12 +43,28 @@ function generateGrid (number){
     assignBoxes();
  }
 
+ function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 
 function assignBoxes(){
     boxes.forEach((box) => {
         box.addEventListener("mouseenter", (e) => {
-            e.target.style.background = "black";
+            if(e.target.style["background-color"] == "white"){
+            e.target.style.background = getRandomColor();
+            } else {
+                if(e.target.firstChild.style.opacity <= 1){
+                e.target.firstChild.style.opacity = +e.target.firstChild.style.opacity + 0.1;
+                }
+            }
         })
     })
 }
+
 
